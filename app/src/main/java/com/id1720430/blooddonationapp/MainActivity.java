dc666844d522b2c3c8e5d67e70905174d923ce0d
Nabil160 <nbakhunjee97@gmail.com>
2021-12-24 18:43:48 +0600

    fetch data on nav drawer
    show profile data on navigation drawer.

--- app/src/main/java/com/id1720430/blooddonationapp/MainActivity.java
+++ app/src/main/java/com/id1720430/blooddonationapp/MainActivity.java
@@ -1,19 +1,35 @@
 package com.id1720430.blooddonationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     protected NavigationView navigationView;
+
+    private CircleImageView nav_profile_image;
+    private TextView nav_fullName, nav_email, nav_phoneNumber, nav_homeAddress, nav_bloodGroup;
+
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +48,42 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        nav_profile_image = navigationView.getHeaderView(0).findViewById(R.id.nav_user_image);
+        nav_fullName = navigationView.getHeaderView(0).findViewById(R.id.nav_user_fullname);
+        nav_email = navigationView.getHeaderView(0).findViewById(R.id.nav_user_email);
+        nav_phoneNumber = navigationView.getHeaderView(0).findViewById(R.id.nav_user_phonenumber);
+        nav_homeAddress = navigationView.getHeaderView(0).findViewById(R.id.nav_user_homeaddress);
+        nav_bloodGroup = navigationView.getHeaderView(0).findViewById(R.id.nav_user_bloodgroup);
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(
+                FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String name = snapshot.child("name").getValue().toString();
+                    nav_fullName.setText(name);
+                    String email = snapshot.child("email").getValue().toString();
+                    nav_email.setText(email);
+                    String phonenumber = snapshot.child("phonenumber").getValue().toString();
+                    nav_phoneNumber.setText(phonenumber);
+                    String homeaddress = snapshot.child("homeaddress").getValue().toString();
+                    nav_homeAddress.setText(homeaddress);
+                    String bloodgroup = snapshot.child("bloodgroup").getValue().toString();
+                    nav_bloodGroup.setText(bloodgroup);
+
+                    String profilepictureurl = snapshot.child("profilepictureurl").getValue().toString();
+                    Glide.with(getApplicationContext()).load(profilepictureurl).into(nav_profile_image);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
