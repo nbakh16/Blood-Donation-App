f83aeb41ea7af942864369d11d3ef82fa683798b
Nabil160 <nbakhunjee97@gmail.com>
2021-12-25 19:23:55 +0600

    Added about app page

---
+++ app/src/main/java/com/nbakh/blooddonation/AboutActivity.java
@@ -0,0 +1,29 @@
+package com.nbakh.blooddonation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private Button homeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(AboutActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/nbakh/blooddonation/MainActivity.java
+++ app/src/main/java/com/nbakh/blooddonation/MainActivity.java
@@ -84,10 +84,7 @@ public class MainActivity extends AppCompatActivity
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-              //String type = snapshot.child("type").getValue().toString();
-                //readDonors();
                 readRecipients();
-
             }
 
             @Override
@@ -142,34 +139,6 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-//    private void readDonors() {
-//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
-//                .child("users");
-//        Query query = reference.orderByChild("type").equalTo("donor");
-//        query.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                userList.clear();
-//                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-//                    User user = dataSnapshot.getValue(User.class);
-//                    userList.add(user);
-//                }
-//                userAdapter.notifyDataSetChanged();
-//                progressbar.setVisibility(View.GONE);
-//
-//                if (userList.isEmpty()){
-//                    Toast.makeText(MainActivity.this, "No recipients", Toast.LENGTH_SHORT).show();
-//                    progressbar.setVisibility(View.GONE);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
-//    }
-
     private void readRecipients() {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
                 .child("users");
@@ -195,60 +164,63 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-
-            case R.id.aplus:
-                Intent intent3 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent3.putExtra("group", "A+");
-                startActivity(intent3);
-                break;
-
-            case R.id.aminus:
-                Intent intent4 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent4.putExtra("group", "A-");
-                startActivity(intent4);
-                break;
-            case R.id.bplus:
-                Intent intent5 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent5.putExtra("group", "B+");
-                startActivity(intent5);
-                break;
-
-            case R.id.bminus:
-                Intent intent6 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent6.putExtra("group", "B-");
-                startActivity(intent6);
-                break;
-            case R.id.abplus:
-                Intent intent7 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent7.putExtra("group", "AB+");
-                startActivity(intent7);
-                break;
-            case R.id.abminus:
-                Intent intent8 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent8.putExtra("group", "AB-");
-                startActivity(intent8);
-                break;
-            case R.id.oplus:
-                Intent intent9 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent9.putExtra("group", "O+");
-                startActivity(intent9);
-                break;
-            case R.id.ominus:
-                Intent intent10 = new Intent(MainActivity.this, CategorySelectedActivity.class);
-                intent10.putExtra("group", "O-");
-                startActivity(intent10);
-                break;
-
             case R.id.profile:
                 Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
                 break;
-
             case R.id.logout:
                 FirebaseAuth.getInstance().signOut();
                 Intent intent2 = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent2);
                 break;
+
+            case R.id.Apos:
+                Intent intent3 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent3.putExtra("group", "A+");
+                startActivity(intent3);
+                break;
+
+            case R.id.Aneg:
+                Intent intent4 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent4.putExtra("group", "A-");
+                startActivity(intent4);
+                break;
+            case R.id.Bpos:
+                Intent intent5 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent5.putExtra("group", "B+");
+                startActivity(intent5);
+                break;
+
+            case R.id.Bneg:
+                Intent intent6 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent6.putExtra("group", "B-");
+                startActivity(intent6);
+                break;
+            case R.id.ABpos:
+                Intent intent7 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent7.putExtra("group", "AB+");
+                startActivity(intent7);
+                break;
+            case R.id.ABneg:
+                Intent intent8 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent8.putExtra("group", "AB-");
+                startActivity(intent8);
+                break;
+            case R.id.Opos:
+                Intent intent9 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent9.putExtra("group", "O+");
+                startActivity(intent9);
+                break;
+            case R.id.Oneg:
+                Intent intent10 = new Intent(MainActivity.this, CategorySelectedActivity.class);
+                intent10.putExtra("group", "O-");
+                startActivity(intent10);
+                break;
+            case R.id.about:
+                Intent intent11 = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent11);
+                break;
+
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
